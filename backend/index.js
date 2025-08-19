7c89fbb210df2c5202c589d497d4aab25241fe39
juan13335 <juanpablofarias26@gmail.com>
2025-08-18 21:45:56 -0300

    back

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@
     import sequelize from './database/bd.js';
     import productosRoutes from './routes/productoRoutes.js';
     import categoriasRoutes from './routes/categoriaRoutes.js';
+    import cors from 'cors';
 
     const app = express();
     const PORT = 3000;
     app.use(express.json());
+    app.use(cors());
+
 
     app.use('/api', productosRoutes);
     app.use('/api', categoriasRoutes);
